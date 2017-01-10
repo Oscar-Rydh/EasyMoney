dd46d120713990a31d5e07eeceb32a7d83a0febc
oscar-rydh <oscar.rydh.93@gmail.com>
2017-01-10 20:40:38 +0100

    Changed the name on routes directory to api

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var aktier = require('./routes/aktier');
+var aktier = require('./api/aktier');
 var app = express();
 
 var db = 'aktier'
